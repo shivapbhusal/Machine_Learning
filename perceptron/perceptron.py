a6cecd83c000e40e62f5a9eef80c418c7184889d
shivapbhusal <civabhusal@gmail.com>
2018-02-15 18:25:06 -0500

    finalize perceptron

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,72 +1,40 @@
-# Python Implementation of Perceptron 
-# Adapted from Python Machine Learning, Second Edition, Sebastian Raschka 
-
-import numpy as np
-
-class Perceptron(object):
-    """Perceptron classifier.
-
-    Parameters
-    ------------
-    eta : float
-      Learning rate (between 0.0 and 1.0)
-    n_iter : int
-      Passes over the training dataset.
-    random_state : int
-      Random number generator seed for random weight
-      initialization.
-
-    Attributes
-    -----------
-    w_ : 1d-array
-      Weights after fitting.
-    errors_ : list
-      Number of misclassifications (updates) in each epoch.
-
-    """
-    def __init__(self, eta=0.01, n_iter=50, random_state=1):
-        self.eta = eta
-        self.n_iter = n_iter
-        self.random_state = random_state
-
-    def fit(self, X, y):
-        """Fit training data.
-
-        Parameters
-        ----------
-        X : {array-like}, shape = [n_samples, n_features]
-          Training vectors, where n_samples is the number of 
-          samples and
-          n_features is the number of features.
-        y : array-like, shape = [n_samples]
-          Target values.
-
-        Returns
-        -------
-        self : object
-
-        """
-        rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01, 
-                              size=1 + X.shape[1])
-        self.errors_ = []
-
-        for _ in range(self.n_iter):
-            errors = 0
-            for xi, target in zip(X, y):
-                update = self.eta * (target - self.predict(xi))
-                self.w_[1:] += update * xi
-                self.w_[0] += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
-        return self
-
-    def net_input(self, X):
-        """Calculate net input"""
-        return np.dot(X, self.w_[1:]) + self.w_[0]
-
-    def predict(self, X):
-        """Return class label after unit step"""
-        return np.where(self.net_input(X) >= 0.0, 1, -1)
+# Python implementation of a simple Perceptron 
+#Author: Shiva Bhusal 
+# Acknowledgement: This program has been partly inspired from the Java implementation discussed in https://www.youtube.com/watch?v=4aksMtJHWEQ&t=623s
 
 
+def calculateOutput(theta,weights, x,y):
+    sum=x*weights[0]+y*weights[1]+weights[2]
+    if sum>=theta:
+        return 1.0
+    else:
+        return 0.0
+
+THETA=0.1
+INSTANCES=8
+LEARNING_RATE=0.5
+MAX_ITER=8 
+X=[1,2.5,2,4,-1,-2.5,-2,-4]
+Y=[1,2.5,3,5,-1,-2.5,-3,-5]
+
+weights=[0.2,0.2,0.2] 
+outputs=[0,0,0,0,1,1,1,1]
+globalError=10
+iteration=0
+
+while globalError!=0 and iteration<=MAX_ITER:
+    globalError=0
+    for i in range(INSTANCES):
+        output=calculateOutput(THETA,weights,float(X[i]),float(Y[i]))
+        localError=float(outputs[i])-output
+        weights[0]=float(weights[0])+LEARNING_RATE*localError*X[i]
+        weights[1]=float(weights[1])+LEARNING_RATE*localError*Y[i]
+        weights[2]=float(weights[2])+LEARNING_RATE*localError
+        globalError=globalError+localError*localError
+    
+    iteration=iteration+1
+
+print(weights)    
+
+print(calculateOutput(THETA,weights,3.5,3.5))
+print(calculateOutput(THETA, weights,-3.5,-3.5))
