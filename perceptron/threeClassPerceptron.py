97c9b084e7abd82577d62690f57f56e8e07a2c6f
shivapbhusal <civabhusal@gmail.com>
2018-02-16 14:32:22 -0500

    modify three class problem

--- perceptron/threeClassPerceptron.py
+++ perceptron/threeClassPerceptron.py
@@ -36,8 +36,27 @@ def findWeights(X,Y):
         iteration=iteration+1
     return weights    
 
-X=[1,2.5,2,4,-1,-2.5,-2,-4]
-Y=[1,2.5,3,5,-1,-2.5,-3,-5]
+XAB=[4.5,4,5.5,6,-2,-2.5,-2,-3]
+YAB=[6,7,6,6.5,-2.5,-2.5,-3,-4.5]
 
-print(classify(calculateOutput(THETA,findWeights(X,Y),3.5,3.5)))
-print(classify(calculateOutput(THETA,findWeights(X,Y),-3.5,-3.5)))
+XBC=[-2,-2.5,-2,-3,-6.5,-7,-6,-8]
+YBC=[-2.5,-2.5,-3,-4.5,8,7.5,10,8.5]
+
+XCA=[-6.5,-7,-6,-8,4.5,4,5.5,6]
+YCA=[8,7.5,10,8.5,6,7,6,6.5]
+
+NewPoints=[[]]
+
+def findClass(x,y):
+    if calculateOutput(THETA,findWeights(XAB,YAB),3.5,3.5)>calculateOutput(THETA,findWeights(XBC,YBC),3.5,3.5) and calculateOutput(THETA,findWeights(XAB,YAB),3.5,3.5)>calculateOutput(THETA,findWeights(XCA,YCA),3.5,3.5):
+        print("Class A")
+    elif calculateOutput(THETA,findWeights(XBC,YBC),3.5,3.5)>calculateOutput(THETA,findWeights(XAB,YAB),3.5,3.5) and calculateOutput(THETA,findWeights(XBC,YBC),3.5,3.5):
+        print("Class B")
+    else:
+        print("Class C") 
+
+
+NewPoints=[[4,6],[-3,-4],[-8,9.5]]
+
+for x,y in NewPoints:
+    findClass(x,y)
