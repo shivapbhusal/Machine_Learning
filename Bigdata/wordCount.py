345f8c74dbca4f4983f30d74f135d6fbed360076
shivapbhusal <civabhusal@gmail.com>
2018-02-24 02:25:08 -0500

    add average no of lines in the program

--- Bigdata/wordCount.py
+++ Bigdata/wordCount.py
@@ -1,13 +1,13 @@
+import numpy as np
 from mrjob.job import MRJob
-
 
 class MRWordFrequencyCount(MRJob):
 
     def mapper(self, _, line):
-        print(line[0:5])
         yield line[0:5], len(line.split())
 
     def reducer(self, key, values):
+        yield key, np.average(list(values))
         yield key, sum(values)
 
 if __name__ == '__main__':
